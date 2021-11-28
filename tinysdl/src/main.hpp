d725ead2cd979a522e0c68a3153280c75925b6dc
silent1603 <tamgiangbui@gmail.com>
2021-11-28 19:32:55 +0700

    update cmake and git

--- tinysdl/src/main.hpp
+++ tinysdl/src/main.hpp
@@ -1,1 +1,21 @@
- 
+#ifndef MAIN_HPP
+#define MAIN_HPP 
+#define GLM_FORCE_RADIANS 1
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glad/glad.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_mixer.h> 
+#include <SDL2/SDL_ttf.h>
+// C++
+#include <vector>
+#include <glad/glad.h>
+// Dear ImGui
+#include <imgui_impl_sdl.h>
+#include <imgui_impl_opengl3.h>
+#include <string>
+#include <iostream>
+
+#endif
