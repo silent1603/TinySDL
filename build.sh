09b5a97a291f1b3484c9a0752d3b1ebaa997a427
silent1603 <tamgiangbui@gmail.com>
2022-01-23 19:45:52 +0700

    - Update build linux

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 if [ ! -d "./build" ]; then
     mkdir build
+fi
+git submodule update --init --recursive
+if [ ! -e "./vcpkg/vcpkg" ]; then
+    echo isntall vcpkg
+     ./vcpkg/bootstrap-vcpkg.sh
 fi
 cd build 
 cmake .. 
