57761cd6630babcdfe807ece61f5a8af90d65520
silent1603 <tamgiangbui@gmail.com>
2022-03-02 23:37:31 +0700

    update shader

--- tinysdl/Game/Tetris/Shader.h
+++ tinysdl/Game/Tetris/Shader.h
@@ -1,8 +1,10 @@
 #ifndef SHADER_H
 #define SHADER_H
-
+#include <iostream>
 class Shader
 {
+public:
+
 	const char* s_vRedBlock = "#version 330 core \n"
 		"layout(location = 0) in vec3 aPos; \n"
 		"void main() {\n"
@@ -69,6 +71,26 @@ class Shader
 		"void main() {\n"
 		" FragColor =vec4(1.0,0.0,0.0,1.0);\n"
 		"}";
+
+	const char* s_vBackgroundBlock = "#version 330 core \n"
+		"layout(location = 0) in vec3 aPos; \n"
+		"void main() {\n"
+		" gl_Postion =vec4(aPos.x,aPos.y,aPos.z,1.0);\n"
+		"}";
+	const char* s_fBackgroundBlock = "#version 330 core \n"
+		"out vec4 FragColor; \n"
+		"void main() {\n"
+		" FragColor =vec4(1.0,0.0,0.0,1.0);\n"
+		"}";
+
+	Shader();
+	void compileShader(std::string vs, std::string fs);
+	void compileProgram(std::string vs, std::string fs);
+
+private:
+	uint32_t m_fShader;
+	uint32_t m_vShader;
+	uint32_t m_programShader;
 };
 
 
